8ef9b0105b10ef526f7525352495f9556daa86e3
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-12-10 18:16:35 +0100

    add item module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { ItemsModule } from './items/items.module';
 
 @Module({
-    imports    : [],
+    imports: [
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+            driver        : ApolloDriver,
+            playground    : false,
+            autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+            plugins       : [
+                ApolloServerPluginLandingPageLocalDefault(),
+            ],
+        }),
+        ItemsModule,
+    ],
     controllers: [],
     providers  : [],
 })
