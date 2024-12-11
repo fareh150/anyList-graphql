88642a698ab81c4cc9a324f700c39d7187a8b4ed
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-12-11 07:53:17 +0100

    end basic configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ConfigModule } from '@nestjs/config';
 import { join } from 'path';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { ItemsModule } from './items/items.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
     imports: [
+        ConfigModule.forRoot(),
         GraphQLModule.forRoot<ApolloDriverConfig>({
             driver        : ApolloDriver,
             playground    : false,
@@ -14,6 +17,16 @@ import { ItemsModule } from './items/items.module';
             plugins       : [
                 ApolloServerPluginLandingPageLocalDefault(),
             ],
+        }),
+        TypeOrmModule.forRoot({
+            type            : 'postgres',
+            host            : process.env.DB_HOST,
+            port            : +process.env.DB_PORT,
+            username        : process.env.DB_USERNAME,
+            password        : process.env.DB_PASSWORD,
+            database        : process.env.DB_NAME,
+            synchronize     : true, // DO NOT USE IN PRODUCTION
+            autoLoadEntities: true,
         }),
         ItemsModule,
     ],
