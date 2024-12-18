c84594256547806adc1096743170dca0b3af78db
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-12-18 09:01:42 +0100

    fix password error

--- src/auth/decorators/current-user.decorator.ts
+++ src/auth/decorators/current-user.decorator.ts
@@ -5,6 +5,7 @@ export const CurrentUser = createParamDecorator(
     ( roles = [], context: ExecutionContext) => {
         const ctx = GqlExecutionContext.create(context);
         const user = ctx.getContext().req.user;
+        delete user.password;
 
         if (!user) {
             throw new InternalServerErrorException('No user inside the request - make sure that we are using the AuthGuard');
