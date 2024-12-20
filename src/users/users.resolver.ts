f7f75af62532ed750c732f9b4134b9aac68aa3bf
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-12-20 08:30:39 +0100

    and authentication restriction and role filter

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -2,14 +2,20 @@ import { Resolver, Query, Mutation, Args, ID } from '@nestjs/graphql';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { ValidRolesArgs } from './dto/roles.arg';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { UseGuards } from '@nestjs/common';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { ValidRoles } from 'src/auth/enums/valid-roles.enum';
 
 @Resolver(() => User)
+@UseGuards(JwtAuthGuard)
 export class UsersResolver {
     constructor(private readonly usersService: UsersService) {}
 
   @Query(() => [User], { name: 'users' })
     async findAll(
       @Args() validRoles:ValidRolesArgs,
+      @CurrentUser([ValidRoles.admin, ValidRoles.superUser]) user: User,
     ): Promise<User[]> {
         return await this.usersService.findAll(validRoles.roles);
     }
