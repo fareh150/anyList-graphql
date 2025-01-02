be9238c04d6142d721e271dfe5459270fa309b33
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-01-02 09:20:39 +0100

    edit list item usage

---
+++ src/list-item/entities/list-item.entity.ts
@@ -0,0 +1,33 @@
+import { ObjectType, Field, ID } from '@nestjs/graphql';
+import { Item } from 'src/items/entities/item.entity';
+import { List } from 'src/lists/entities/list.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('listItems')
+@ObjectType()
+export class ListItem {
+
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => ID)
+      id: string;
+
+  @Column({ type: 'numeric' })
+  @Field(() => Number)
+      quantity: number;
+
+  @Column({ type: 'boolean' })
+  @Field(() => Boolean)
+      completed: boolean;
+
+  @Column({ type: 'date' })
+  @Field(() => Date, { nullable: true })
+      endDate?: Date;
+
+  @ManyToOne(() => List, list => list.listItem, { lazy: true })
+  // No ponemos field para poder hacer paginacion
+      list: List;
+
+  @ManyToOne(() => Item, item => item.listItem, { lazy: true })
+  @Field(() => Item)
+      item: Item;
+}
